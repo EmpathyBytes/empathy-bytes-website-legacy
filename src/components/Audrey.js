eea490bf43f50cdca135bce8a4f979920f11668f
Chung, Audrey <achung63@gatech.edu>
2022-10-09 17:36:02 -0400

    Create Audrey.js

---
+++ src/components/Audrey.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+const Auds = () => {
+    return (
+      <div>
+        <h1> Audrey </h1>
+        <p> welcome to my file </p>
+      </div>
+    );
+  };
+  
+  export default Auds;
