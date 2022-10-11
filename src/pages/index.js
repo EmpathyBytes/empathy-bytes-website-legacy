d71c3e94969ee6a5cde471daf24305ca1bf913a4
princessempel <princess.empel@gmail.com>
2022-10-11 11:16:26 -0400

    Added Princess component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import Joseph from "../components/Joseph";
 import Wilson from "../components/wilson";
+import Princess from "../components/Princess";
+
 
 const pageStyles = {
   color: "#232129",
@@ -96,6 +98,7 @@ const IndexPage = () => {
 
       <Wilson></Wilson>
       <Joseph/>
+      <Princess></Princess>
 
       <img
         alt="Gatsby G Logo"
