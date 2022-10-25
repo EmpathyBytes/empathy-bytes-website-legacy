b46e18286495302ee67bd5b59a390eda6e625098
jamin <jacobamin77@gmail.com>
2022-10-24 22:28:13 -0400

    contentful fetch + people cards

---
+++ src/components/PeopleCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const cardContainer = {
+    display: "block",
+}
+
+const card = {
+    background: "#fff",
+    borderRadius: "7px",
+    boxShadow: "0 2px 5px #ccc",
+    padding: "10px",
+    width: "240px",
+    margin: "16px",
+    float: "left",
+}
+
+const name = {
+    fontSize: "2em",
+    marginBottom: "6px"
+}
+
+const role = {
+    fontSize: "1em",
+    marginBottom: "6px",
+    whiteSpace: "pre-wrap",
+    wordWrap: "break-word",
+}
+
+function PeopleCard(props) {
+
+    return (
+        <div style= { card }>
+            <h1 style= {name}> { props.name } </h1>
+            <img src= { props.img }></img>
+            <h3 style={ role }> { props.role } </h3>
+        </div>
+    )
+}
+
+export default PeopleCard;
---
+++ src/pages/people.js
@@ -0,0 +1,73 @@
+import React, {useState, useEffect, useCallback} from "react"
+import NavBar from "../components/navbar";
+import PeopleCard from "../components/PeopleCard"
+import { client } from "../client";
+
+// Temporary page created to house the people card information - Jacob
+
+const PeoplePage = () => {
+
+    const[isLoading, setIsLoading] = useState(false)
+    const[people, setPeople] = useState([]);
+
+
+    // Cleaning up the Data Pulled from Contentful
+    const cleanUpPeople = useCallback((rawData) => {
+        const cleanPeople = rawData.map((people) => {
+            const {sys, fields} = people
+            const {id} = sys
+
+            const personName = fields.name
+            const personImage = fields.image
+            const personRole = fields.role
+
+            const cleaned = {id, personName, personImage, personRole}
+            return cleaned
+        })
+        setPeople(cleanPeople)
+    }, [])
+
+    // Fetching data from contentful
+    const getPeopleInfo = useCallback(async () => {
+        setIsLoading(true)
+        try {
+            const res = await client.getEntries({ content_type: 'members' })
+            const resData = res.items;
+            if (resData) {
+                cleanUpPeople(resData)
+            } else {
+                setPeople([])
+            }
+            setIsLoading(false)
+        } catch (error) {
+            console.log(error)
+            setIsLoading(false)
+        }
+    }, [cleanUpPeople])
+
+    useEffect(() => {
+        getPeopleInfo()
+    }, [getPeopleInfo])
+
+    console.log(people)
+
+  return (
+    <div>
+        <NavBar />
+        <div>
+        {/* Iterating and Creating each content card */}
+            {people.map((item) => (
+                <PeopleCard 
+                    name={item.personName}
+                    image={item.personImage}
+                    role={item.personRole}
+                />
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default PeoplePage
+
+export const Head = () => <title>People Test - Jacob</title>
