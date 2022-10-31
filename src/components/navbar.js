66dcb4a3045b806d97ccb36e7af81a366293a108
Joseph Liu <josephliu24@outlook.com>
2022-10-31 01:42:31 -0400

    Made it so the selected button will be bolded when selected

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,9 @@ import {useEffect, useState} from "react";
 
 export default function NavBar() {
     const breakpoint = 80
-    const [outerClass, setOuterClass] = useState("navbar-outer")
+    const [outerClass, setOuterClass] = useState("navbar-outer navbar-top")
+
+    const topLvlPath = () => window.location.pathname.split('/')[1]
 
     const onScroll = () => {
         const scroll = window.scrollY
@@ -23,13 +25,16 @@ export default function NavBar() {
         <header className={outerClass}>
                 <div className="navbar-content">
                         {/* `Icon` is the image file for the site logo */}
-                        <Link className="navbar-link" to='/'>
+                        <Link className={`navbar-link ${topLvlPath()===''?'navbar-selected':''}`} to='/'>
                             <img src={Icon} alt="Logo" style={{height: '3rem'}}/>
                         </Link>
                         {/* Links to other pages goes here! */}
-                        <Link className="navbar-link" to='/projects'>Projects</Link>
-                        <Link className="navbar-link" to='/about'>About</Link>
-                        <Link className="navbar-link" to='/contact'>Contact</Link>
+                        <Link className={`navbar-link ${topLvlPath()==='projects'?'navbar-selected':''}`}
+                              to='/projects'>Projects</Link>
+                        <Link className={`navbar-link ${topLvlPath()==='about'?'navbar-selected':''}`}
+                              to='/about'>About</Link>
+                        <Link className={`navbar-link ${topLvlPath()==='contact'?'navbar-selected':''}`}
+                              to='/contact'>Contact</Link>
                 </div>
         </header>
     )
