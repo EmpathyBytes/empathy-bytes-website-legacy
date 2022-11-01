54142378df302447646d5a05bc238575bd1b4aa5
jamin <jacobamin77@gmail.com>
2022-10-31 23:26:32 -0400

    attempted wordpress add

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,22 @@ module.exports = {
     title: `Empathy Bytes`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
+  plugins: [
+  //   {
+  //   resolve: 'gatsby-source-contentful',
+  //   options: {
+  //     "accessToken": "8-pPEMtOnQgmB8HgnmhQWdlWeRPnuXWXrj4UItYIu5A",
+  //     "spaceId": "d608cqxcd02b"
+  //   }
+  // }
+  {
+    resolve: `gatsby-source-wordpress`,
     options: {
-      "accessToken": "8-pPEMtOnQgmB8HgnmhQWdlWeRPnuXWXrj4UItYIu5A",
-      "spaceId": "d608cqxcd02b"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sitemap", {
+      url:
+        `https://educast.library.gatech.edu/graphql`,
+    },
+  }
+  , "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
