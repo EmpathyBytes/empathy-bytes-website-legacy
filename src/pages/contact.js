d3b270aa336ad56bd2d25a3d7ccb1469226dda23
Chung, Audrey <achung63@gatech.edu>
2022-11-01 11:34:58 -0400

    fixed basic contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,58 @@
-import * as React from "react";
+import React, { useContext, useEffect } from "react";
+//import { FormCtx } from "./Form";
 import '../styles.css';
 import NavBar from "../components/navbar";
 
-// how do i put fields in?
-const contactBox = {
-
-};
+//// how do i put fields in?
+//const contactBox = props => {
+//    const {id} = props;
+//    const {setFields, addField} = useContext(FormCtx);
+//    const {
+//        value,
+//        classes,
+//        validate,
+//        placeholder,
+//        label = "",
+//        event = {}
+//    } = field;
+//
+//    const {onChange, ...restEvents} = events;
+//    const {contClass, fieldClass, errorClass} = classes;
+//
+//    const handleChange = event => {
+//        setFields(event, field);
+//
+//        if (typeof onChange === "function") {
+//            onChange({
+//                ...field,
+//                value: event.target.value
+//            });
+//        }
+//    };
+//
+//    useEffect(() => {
+//        addField({
+//        field: props,
+//        value
+//        });
+//        }, []);
+//    return (
+//    <div classname = {contClass}>
+//    {label}
+//    <input
+//            {...restEvents}
+//            id = {id}
+//            type="text"
+//            value={value}
+//            class={fieldClass}
+//            validate={validate}
+//            onChange = {handleChange}
+//            placeholder={placeholder}
+//          />
+//          <p className = {errorClass}></p>
+//    </div>
+//    );
+//};
 
 const pageStyles = {
   color: "#232129",
@@ -33,9 +80,36 @@ const contactPage = () => {
            Contact Us
        </span>
            </h1>
+           <div>
+           <form method="post" action="#">
+             <label>
+               Name <br/>
+               <input type="text" name="name" id="name" />
+             </label>
+             <br/>
+             <label>
+               Email <br/>
+               <input type="email" name="email" id="email" />
+             </label>
+             <br/>
+             <label>
+               Subject <br/>
+               <input type="text" name="subject" id="subject" />
+             </label>
+             <br/>
+             <label>
+               Message <br/>
+               <textarea name="message" id="message" rows="5" />
+             </label>
+             <br/>
+             <button type="submit">Send</button>
+             <input type="reset" value="Clear" />
+           </form>
+           </div>
            </main>
        </div>
    );
 };
 
 export default contactPage;
+//export default contactBox;
