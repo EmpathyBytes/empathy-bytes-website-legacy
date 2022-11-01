85cdaf5b8af6d033b03855852d6beb8d9a07f1d9
jamin <jacobamin77@gmail.com>
2022-11-01 11:33:29 -0400

    changed the gatsby config to work with wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,38 +4,44 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
-  //   {
-  //   resolve: 'gatsby-source-contentful',
-  //   options: {
-  //     "accessToken": "8-pPEMtOnQgmB8HgnmhQWdlWeRPnuXWXrj4UItYIu5A",
-  //     "spaceId": "d608cqxcd02b"
-  //   }
-  // }
-  {
-    resolve: `gatsby-source-wordpress`,
-    options: {
-      url:
-        `https://educast.library.gatech.edu/graphql`,
-    },
-  }
-  , "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+    //   {
+    //   resolve: 'gatsby-source-contentful',
+    //   options: {
+    //     "accessToken": "8-pPEMtOnQgmB8HgnmhQWdlWeRPnuXWXrj4UItYIu5A",
+    //     "spaceId": "d608cqxcd02b"
+    //   }
+    // }
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: `https://educast.library.gatech.edu/graphql`,
+      },
+      type: {
+        MediaItem: {
+          localFile: {
+            maxFileSizeBytes: 0, // 10Mb. Set it to 0 to avoid downloads
+          },
+        },
+      },
+    }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sitemap", {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    }, "gatsby-plugin-mdx", "gatsby-transformer-remark", {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }, {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     }
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  ]
 };
