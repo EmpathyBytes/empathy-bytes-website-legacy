b339098822766c3fa9f176ff51ac88c367cf04aa
Wilson Chen <36680838+KeroseneTrampolines@users.noreply.github.com>
2022-11-15 11:19:35 -0500

    Fix Project image loading

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,54 +1,61 @@
 import * as React from "react";
-import '../styles.css';
+import "../styles.css";
 import NavBar from "../components/navbar";
-import Project from "../components/Project"
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import Project from "../components/Project";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import { client } from "../client";
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby";
 
 const container = {
-  width: '80%',
+  width: "80%",
   padding: 96,
-  paddingTop: 150
-}
+  paddingTop: 150,
+};
 
 const ProjectsPage = () => {
-
   const data = useStaticQuery(graphql`
     query newQuery {
-      allWpPost (filter: {categories: {nodes: {elemMatch: {slug: {eq: "interviews"}}}}}) {
+      allWpPost(
+        filter: {
+          categories: { nodes: { elemMatch: { slug: { eq: "interviews" } } } }
+        }
+      ) {
         nodes {
           title
           featuredImage {
-              node {
-                mediaItemUrl
-              }
+            node {
+              mediaItemUrl
+            }
           }
         }
       }
     }
-  `)
+  `);
 
   const arr = data.allWpPost.nodes;
 
-   return(
-       <div style={container}>
-       <NavBar/>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2}>
+  return (
+    <div style={container}>
+      <NavBar />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
           {arr.map((item) => (
             <Grid Project xs={6} padding={2}>
-                <Project 
-                    title={item.title}
-                    //image={item.featuredImage.node.mediaItemUrl}
-                />
-                </Grid>
-            ))}
-          </Grid>
+              <Project
+                title={item.title}
+                image={
+                  item.featuredImage != null
+                    ? item.featuredImage.node.mediaItemUrl
+                    : false
+                }
+              />
+            </Grid>
+          ))}
+        </Grid>
       </Box>
-       </div>
-   );
+    </div>
+  );
 };
 
 export default ProjectsPage;
