d5111d0d66f7f79ee6486e5da1d07248e673e65e
Wilson Chen <36680838+KeroseneTrampolines@users.noreply.github.com>
2022-12-05 14:24:56 -0500

    Updates

--- src/client.js
+++
@@ -1,8 +0,0 @@
-import * as contentful from 'contentful'
-
-export const client = contentful.createClient({
-    // space: process.env.APP_SPACE_ID,
-    // accessToken: process.env.APP_ACCESS_TOKEN
-    space: 'd608cqxcd02b',
-    accessToken: '8-pPEMtOnQgmB8HgnmhQWdlWeRPnuXWXrj4UItYIu5A'
-})
